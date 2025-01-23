1fa329e0c9eaf4c6162c47474c43979f665f356c
MatosRicardo <ricardoribeirodematos@gmail.com>
2025-01-23 16:25:59 -0300

    refactor: simple code change

--- script.js
+++ script.js
@@ -4,13 +4,23 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let pontos = 20;
 let pontuacao = 0;
 
+const atualizarMensagem = (mensagem) => {
+  document.querySelector(".message").textContent = mensagem;
+};
+
+const atualizarEstiloDerrota = () => {
+  atualizarMensagem("Você perdeu!😭");
+  document.querySelector(".score").textContent = 0;
+  document.querySelector("body").style.backgroundColor = "#ff0000";
+};
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   if (!guess) {
-    document.querySelector(".message").textContent = "Sem número!⛔";
+    atualizarMensagem("Sem número!⛔");
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "Acertou!🎉";
+    atualizarMensagem("Acertou!🎉");
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
@@ -19,35 +29,12 @@ document.querySelector(".check").addEventListener("click", function () {
       pontuacao = pontos;
       document.querySelector(".highscore").textContent = pontuacao;
     }
-  } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Muito alto!📈";
-    if (pontos > 1) {
-      pontos--;
-      document.querySelector(".score").textContent = pontos;
-    } else {
-      document.querySelector(".message").textContent = "Você perdeu!😭";
-      document.querySelector(".score").textContent = 0;
-      document.querySelector("body").style.backgroundColor = "#ff0000";
-    }
-  } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "Muito baixo!📉";
-    if (pontos > 1) {
-      pontos--;
-      document.querySelector(".score").textContent = pontos;
-    } else {
-      document.querySelector(".message").textContent = "Você perdeu!😭";
-      document.querySelector(".score").textContent = 0;
-      document.querySelector("body").style.backgroundColor = "#ff0000";
-    }
-  }
-});
+  } else {
+    const mensagem = guess > secretNumber ? "Muito alto!📈" : "Muito baixo!📉";
+    atualizarMensagem(mensagem);
 
-document.querySelector(".again").addEventListener("click", function () {
-  pontos = 20;
-  document.querySelector(".score").textContent = pontos;
-  document.querySelector(".message").textContent = "Comece a adivinhar...";
-  document.querySelector(".number").textContent = "?";
-  document.querySelector(".guess").value = "";
-  document.querySelector("body").style.backgroundColor = "#222";
-  document.querySelector(".number").style.width = "15rem";
+    pontos > 1
+      ? (document.querySelector(".score").textContent = --pontos)
+      : atualizarEstiloDerrota();
+  }
 });
